3f6d3d4dd3893548f2bb3c3eedebe980d68b2631
Michelle Madeira <michellemadeira1@gmail.com>
2022-03-24 19:34:40 -0300

    Login

---
+++ BlogPessoal/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', // caminho do seletor 
+  templateUrl: './app.component.html',// templete
+  styleUrls: ['./app.component.css'] //estilo css
+})
+export class AppComponent {
+  title = 'BlogPessoal';
+}
---
+++ BlogPessoal/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { HomeComponent } from './home/home.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    HomeComponent,
+    EntrarComponent,
+    CadastrarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
